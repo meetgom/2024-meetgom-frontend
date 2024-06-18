7047d4e669c8637bd716e531f833e185bc19aed9
s00ngle <dydtns9815@gmail.com>
2024-06-18 21:22:33 +0900

    remove shared directory

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,9 +2,11 @@ import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../shared/components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    // '../stories/**/*.mdx',
+    // '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    // '../shared/components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../**/*.mdx',
+    '../**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-onboarding',
