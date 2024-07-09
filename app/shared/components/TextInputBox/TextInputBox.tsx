79f8b8125eef68e7a217ab8112e8bf03e6365ac5
s00ngle <dydtns9815@gmail.com>
2024-07-09 13:15:34 +0900

    feat: Implement TextInputBox component

---
+++ app/shared/components/TextInputBox/TextInputBox.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+interface TextInputBoxProps {
+    title: string
+    placeholder: string
+    onChange: (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => void
+}
+
+export const TextInputBox = ({
+    title,
+    placeholder,
+    onChange,
+    ...props
+}: TextInputBoxProps) => {
+    return (
+        <div>
+            <div
+                className='text-sm text-[#959595] mb-1 ml-2'>
+                {title}
+            </div>
+            <input
+                type="text"
+                className="bg-white text-black border border-[#EFEFEF] placeholder:text-black focus:outline-black font-Pretendard font-semibold rounded-md inline-block leading-none w-80 h-12 p-3"
+                placeholder={placeholder}
+                onChange={onChange}
+                {...props}
+            />
+        </div>
+    )
+}
