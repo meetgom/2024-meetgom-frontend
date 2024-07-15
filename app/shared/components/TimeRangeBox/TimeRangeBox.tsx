ac6b9ac66e458bae11b1884fe7a098d735c513a9
s00ngle <dydtns9815@gmail.com>
2024-07-15 17:09:20 +0900

    improv: Time Range Box re-design

--- app/shared/components/TimeRangeBox/TimeRangeBox.tsx
+++ app/shared/components/TimeRangeBox/TimeRangeBox.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { Button } from '../Button/Button'
 
 interface TimeRangeBoxProps {
   title: string
   placeholder: string
   onChange: (startTime: string, endTime: string) => void
+}
+
+const generateHourOptions = () => {
+  const options = []
+  for (let i = 0; i <= 23; i++) {
+    options.push(i < 10 ? `0${i}` : `${i}`)
+  }
+  return options
+}
+
+const generateMinuteOptions = () => {
+  const options = []
+  for (let i = 0; i <= 55; i += 5) {
+    options.push(i < 10 ? `0${i}` : `${i}`)
+  }
+  return options
 }
 
 export const TimeRangeBox = ({
@@ -13,114 +29,127 @@ export const TimeRangeBox = ({
   onChange,
   ...props
 }: TimeRangeBoxProps) => {
-  const [showModal, setShowModal] = useState(false)
-  const [startTime, setStartTime] = useState('00:00')
-  const [endTime, setEndTime] = useState('00:00')
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false)
+  const [startHour, setStartHour] = useState('09')
+  const [startMinute, setStartMinute] = useState('30')
+  const [endHour, setEndHour] = useState('22')
+  const [endMinute, setEndMinute] = useState('30')
 
-  const handleStartTimeChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const newStartTime = `${event.target.value}:${startTime.split(':')[1]}`
-    setStartTime(newStartTime)
-    onChange(newStartTime, endTime)
-  }
+  const [tempStartHour, setTempStartHour] = useState(startHour)
+  const [tempStartMinute, setTempStartMinute] = useState(startMinute)
+  const [tempEndHour, setTempEndHour] = useState(endHour)
+  const [tempEndMinute, setTempEndMinute] = useState(endMinute)
 
-  const handleStartMinuteChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const newStartTime = `${startTime.split(':')[0]}:${event.target.value}`
-    setStartTime(newStartTime)
-    onChange(newStartTime, endTime)
-  }
+  const hours = generateHourOptions()
+  const minutes = generateMinuteOptions()
 
-  const handleEndTimeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const newEndTime = `${event.target.value}:${endTime.split(':')[1]}`
-    setEndTime(newEndTime)
-    onChange(startTime, newEndTime)
-  }
-
-  const handleEndMinuteChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const newEndTime = `${endTime.split(':')[0]}:${event.target.value}`
-    setEndTime(newEndTime)
-    onChange(startTime, newEndTime)
+  const handleDropdownVisibility = (visible: boolean) => {
+    if (visible) {
+      setTempStartHour(startHour)
+      setTempStartMinute(startMinute)
+      setTempEndHour(endHour)
+      setTempEndMinute(endMinute)
+    }
+    setIsDropdownVisible(visible)
   }
 
   return (
-    <div>
+    <div className="flex flex-col">
       <div className="text-sm text-[#959595] mb-1 ml-2">{title}</div>
       <button
-        className="px-4 py-2  w-80 h-12 rounded border border-[#EFEFEF] rounded-md px-3"
-        onClick={() => setShowModal(true)}
+        className="px-4 py-2 w-80 h-12 rounded border border-[#EFEFEF] rounded-md px-3"
+        onClick={() => handleDropdownVisibility(!isDropdownVisible)}
       >
         <div className="flex items-center justify-between">
-          <div className="ml-3">{startTime}</div>
+          <div className="w-32 flex justify-start">
+            {startHour}시 {startMinute}분
+          </div>
           <div className="text-[#959595] text-sm">{placeholder}</div>
-          <div className="mr-3">{endTime}</div>
+          <div className="w-32 flex justify-end">
+            {endHour}시 {endMinute}분
+          </div>
         </div>
       </button>
-      {showModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-4 rounded">
+      {isDropdownVisible && (
+        <div className="shadow mt-2 w-80 rounded border border-[#EFEFEF] rounded-md">
+          <div className="h-40 flex justify-between px-12">
             <div className="flex">
-              <div className="mr-4">
-                <div className="font-bold mb-2 text-center">시작 시간</div>
-                <div className="flex space-x-2">
-                  <select
-                    value={startTime.split(':')[0]}
-                    onChange={handleStartTimeChange}
+              <div className="mr-1 overflow-y-scroll h-40 scrollbar-hide">
+                {hours.map((hour) => (
+                  <div
+                    key={`start-hour-${hour}`}
+                    className={`w-10 h-8 items-center flex justify-center px-1 rounded my-1 cursor-pointer ${hour === tempStartHour ? 'text-[#00C27C] bg-[#00C27C]/10' : 'text-[#B2B2B2]'}`}
+                    onClick={() => setTempStartHour(hour)}
                   >
-                    {Array.from({ length: 24 }, (_, i) => (
-                      <option key={i} value={i.toString().padStart(2, '0')}>
-                        {i.toString().padStart(2, '0')}
-                      </option>
-                    ))}
-                  </select>
-                  <select
-                    value={startTime.split(':')[1]}
-                    onChange={handleStartMinuteChange}
-                  >
-                    {Array.from({ length: 60 }, (_, i) => (
-                      <option key={i} value={i.toString().padStart(2, '0')}>
-                        {i.toString().padStart(2, '0')}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                    {hour}
+                  </div>
+                ))}
               </div>
-              <div className="">
-                <div className="font-bold mb-2 text-center">종료 시간</div>
-                <div className="flex space-x-2">
-                  <select
-                    value={endTime.split(':')[0]}
-                    onChange={handleEndTimeChange}
+
+              <div className="overflow-y-scroll h-40 scrollbar-hide">
+                {minutes.map((minute) => (
+                  <div
+                    key={`start-minute-${minute}`}
+                    className={`w-10 h-8 items-center flex justify-center px-1 rounded my-1 cursor-pointer ${minute === tempStartMinute ? 'text-[#00C27C] bg-[#00C27C]/10' : 'text-[#B2B2B2]'}`}
+                    onClick={() => setTempStartMinute(minute)}
                   >
-                    {Array.from({ length: 24 }, (_, i) => (
-                      <option key={i} value={i.toString().padStart(2, '0')}>
-                        {i.toString().padStart(2, '0')}
-                      </option>
-                    ))}
-                  </select>
-                  <select
-                    value={endTime.split(':')[1]}
-                    onChange={handleEndMinuteChange}
-                  >
-                    {Array.from({ length: 60 }, (_, i) => (
-                      <option key={i} value={i.toString().padStart(2, '0')}>
-                        {i.toString().padStart(2, '0')}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                    {minute}
+                  </div>
+                ))}
               </div>
             </div>
-            <button
-              className="w-full mt-4 px-3 py-2 bg-blue-500 text-white rounded"
-              onClick={() => setShowModal(false)}
-            >
-              확인
-            </button>
+            <div className="flex">
+              <div className="mr-1 overflow-y-scroll h-40 scrollbar-hide">
+                {hours.map((hour) => (
+                  <div
+                    key={`end-hour-${hour}`}
+                    className={`w-10 h-8 items-center flex justify-center px-1 rounded my-1 cursor-pointer ${hour === tempEndHour ? 'text-[#00C27C] bg-[#00C27C]/10' : 'text-[#B2B2B2]'}`}
+                    onClick={() => setTempEndHour(hour)}
+                  >
+                    {hour}
+                  </div>
+                ))}
+              </div>
+
+              <div className="overflow-y-scroll h-40 scrollbar-hide">
+                {minutes.map((minute) => (
+                  <div
+                    key={`end-minute-${minute}`}
+                    className={`w-10 h-8 items-center flex justify-center px-1 rounded my-1 cursor-pointer ${minute === tempEndMinute ? 'text-[#00C27C] bg-[#00C27C]/10' : 'text-[#B2B2B2]'}`}
+                    onClick={() => setTempEndMinute(minute)}
+                  >
+                    {minute}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="h-12 flex items-center justify-between px-3">
+            <Button
+              backgroundColor="bg-white"
+              fontColor="text-black"
+              borderColor="border-[#EFEFEF]"
+              label="취소"
+              onClick={() => handleDropdownVisibility(false)}
+            />
+            <Button
+              backgroundColor="bg-black"
+              fontColor="text-white"
+              borderColor="border-black"
+              label="확인"
+              onClick={() => {
+                setIsDropdownVisible(false)
+                setStartHour(tempStartHour)
+                setStartMinute(tempStartMinute)
+                setEndHour(tempEndHour)
+                setEndMinute(tempEndMinute)
+                onChange(
+                  `${tempStartHour}:${tempStartMinute}`,
+                  `${tempEndHour}:${tempEndMinute}`
+                )
+              }}
+            />
           </div>
         </div>
       )}
