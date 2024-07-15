b0a5c6d256a8684ebc4acd5b5463a0979592b690
s00ngle <dydtns9815@gmail.com>
2024-07-15 17:38:07 +0900

    improv: Header component position fixed

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <header className="flex w-full justify-between items-center bg-white px-4 md:px-12 py-6 md:py-9">
+    <header className="flex fixed w-full justify-between items-center bg-white px-4 md:px-12 py-6 md:py-9">
       <Image src="/meetgom_logo.svg" alt="Logo" width={129} height={22} />
     </header>
   )
