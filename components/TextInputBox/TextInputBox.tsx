a0d9f38f80cb1ea6a6c18a28bb8baf98e73d8bd0
s00ngle <dydtns9815@gmail.com>
2024-07-19 09:55:05 +0900

    refactor: remove font-semibold

--- components/TextInputBox/TextInputBox.tsx
+++ components/TextInputBox/TextInputBox.tsx
@@ -17,7 +17,7 @@ export const TextInputBox = ({
       <div className="text-sm text-[#959595] mb-1 ml-2">{title}</div>
       <input
         type="text"
-        className="bg-white text-black border border-[#EFEFEF] placeholder:text-black focus:outline-black font-semibold rounded-md inline-block leading-none w-80 h-12 p-3"
+        className="bg-white text-black border border-[#EFEFEF] placeholder:text-black focus:outline-black rounded-md inline-block leading-none w-80 h-12 p-3"
         placeholder={placeholder}
         onChange={onChange}
         {...props}
