1b1a4831f4258ad34c0a1b6d22f504bdcd55c64e
Dawon00 <dawon000203@gmail.com>
2025-01-18 17:09:09 +0900

    chore: update Storybook dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,17 +2,27 @@ import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: ['../**/*.stories.@(js|jsx|mjs|ts|tsx)', '../**/*.mdx'],
+
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-backgrounds',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+
   staticDirs: ['../public'],
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 }
 export default config
