0994fe96a1450529456a62bcaf367952e6e22af2
김용순 <dydtns9815@gmail.com>
2025-03-31 02:20:06 +0900

    refactor: 레이아웃 분리로 인한 디렉토리 구조 변경

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Button } from '../components/Button/Button'
+import { Button } from '@/components/Button/Button'
 
 export default function Home() {
   return (
